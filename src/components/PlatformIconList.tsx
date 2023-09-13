dc9dcabd46e1912d98aa5e2016bb3f765b563a82
Illia Maksymov <98290303+imksmv@users.noreply.github.com>
2023-09-13 18:31:53 +0100

    Update color of platforms

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -33,7 +33,7 @@ const PlatformIconList = ({ platforms }: Props) => {
   return (
     <HStack my={1}>
       {platforms.map((platform) => (
-        <Icon key={platform.id} as={iconMap[platform.slug]} />
+        <Icon color="gray.500" key={platform.id} as={iconMap[platform.slug]} />
       ))}
     </HStack>
   );
