57c68def998f92bc22026e5c2c1c8b59a4d1057a
Illia Maksymov <98290303+imksmv@users.noreply.github.com>
2023-09-13 18:30:03 +0100

    Get optimzed images

---
+++ src/services/image-url.ts
@@ -0,0 +1,7 @@
+const getCroppedImgUrl = (url: string) => {
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  const croppedUrl = url.slice(0, index) + "crop/600/400/" + url.slice(index);
+  return croppedUrl;
+};
+export default getCroppedImgUrl;
