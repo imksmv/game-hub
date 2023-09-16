474073abb7beea9b211e73e00849e5302321e482
Illia Maksymov <98290303+imksmv@users.noreply.github.com>
2023-09-16 12:01:34 +0100

    Modified: logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,10 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+import logo from "../assets/logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent={"space-between"} mb={4}>
+    <HStack justifyContent={"space-between"} my={4}>
       <Image src={logo} boxSize="60px" />
       <ColorModeSwitch />
     </HStack>
