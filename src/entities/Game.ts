0664b3b3df956ab1642178e58be49fa80f1fea63
Illia Maksymov <98290303+imksmv@users.noreply.github.com>
2023-09-25 19:08:00 +0100

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,8 +1,12 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   name: string;
+  genres: Genre[];
+  publishers: Publisher[];
   slug: string;
   description_raw: string;
   background_image: string;
