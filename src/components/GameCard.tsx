5739a79b6033e2a5214c79ff27b54c35070791d7
Illia Maksymov <98290303+imksmv@users.noreply.github.com>
2023-09-25 22:45:12 +0100

    Refactor: click on card to navigate

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, Flex, Heading, Image } from "@chakra-ui/react";
+import { Box, Card, CardBody, Flex, Heading, Image } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { Game } from "../entities/Game";
 import getCroppedImgUrl from "../services/image-url";
@@ -12,34 +12,36 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card variant="filled">
-      <Image src={getCroppedImgUrl(game.background_image)} />
-      <CardBody>
-        <Flex
-          justifyContent="space-between"
-          columnGap={4}
-          rowGap={2}
-          alignItems="center"
-          wrap="wrap"
-          mb={1}
-        >
-          <PlatformIconList
-            platforms={
-              game.parent_platforms
-                ? game.parent_platforms.map((p) => p.platform)
-                : []
-            }
-          />
-          <CriticScore score={game.metacritic} />
-        </Flex>
-        <Heading fontSize="lg">
-          <Flex alignItems="center" gap={2}>
-            <Link to={"/games/" + game.slug}>{game.name}</Link>
-            <Emoji rating={game.rating_top} />
+    <Link to={"/games/" + game.slug}>
+      <Card variant="filled">
+        <Image src={getCroppedImgUrl(game.background_image)} />
+        <CardBody>
+          <Flex
+            justifyContent="space-between"
+            columnGap={4}
+            rowGap={2}
+            alignItems="center"
+            wrap="wrap"
+            mb={1}
+          >
+            <PlatformIconList
+              platforms={
+                game.parent_platforms
+                  ? game.parent_platforms.map((p) => p.platform)
+                  : []
+              }
+            />
+            <CriticScore score={game.metacritic} />
           </Flex>
-        </Heading>
-      </CardBody>
-    </Card>
+          <Box>
+            <Flex alignItems="center" gap={2}>
+              <Heading size={{ sm: "xl", md: "md" }}>{game.name}</Heading>
+              <Emoji rating={game.rating_top} />
+            </Flex>
+          </Box>
+        </CardBody>
+      </Card>
+    </Link>
   );
 };
 
